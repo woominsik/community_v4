9a592c2000355a3798d12e68534392805db6f51f
woominsik <9737638@naver.com>
2022-08-04 22:50:19 +0900

    등록 처리

--- src/main/java/com/ll/exam/article/controller/ArticleController.java
+++ src/main/java/com/ll/exam/article/controller/ArticleController.java
@@ -4,6 +4,7 @@ import com.ll.exam.Rq;
 import com.ll.exam.annotation.Autowired;
 import com.ll.exam.annotation.Controller;
 import com.ll.exam.annotation.GetMapping;
+import com.ll.exam.annotation.PostMapping;
 import com.ll.exam.article.dto.ArticleDto;
 import com.ll.exam.article.service.ArticleService;
 
@@ -26,5 +27,25 @@ public class ArticleController {
     @GetMapping("/usr/article/write")
     public void showWrite(Rq rq) {
         rq.view("usr/article/write");
+    }
+
+    @PostMapping("/usr/article/write")
+    public void write(Rq rq) {
+        String title = rq.getParam("title", "");
+        String body = rq.getParam("body", "");
+
+        if (title.length() == 0) {
+            rq.historyBack("제목을 입력해주세요.");
+            return;
+        }
+
+        if (body.length() == 0) {
+            rq.historyBack("내용을 입력해주세요.");
+            return;
+        }
+
+        long id = articleService.write(title, body);
+
+        rq.replace("/usr/article/detail/free/%d".formatted(id), "%d번 게시물이 생성 되었습니다.".formatted(id));
     }
 }

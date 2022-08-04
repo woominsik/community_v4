24d69470a73d06cbb8d77d3819f2a8ba5dff43f0
woominsik <9737638@naver.com>
2022-08-04 13:21:57 +0900

    테스트시에 SQL콘솔출력 켜기, articleService.modify 구현

--- src/main/java/com/ll/exam/article/service/ArticleService.java
+++ src/main/java/com/ll/exam/article/service/ArticleService.java
@@ -27,4 +27,8 @@ public class ArticleService {
     public long write(String title, String body, boolean isBlind) {
         return articleRepository.write(title, body, isBlind);
     }
+
+    public void modify(long id, String title, String body, boolean isBlind) {
+        articleRepository.modify(id,title,body,isBlind);
+    }
 }
--- src/test/java/com/ll/exam/ArticleServiceTest.java
+++ src/test/java/com/ll/exam/ArticleServiceTest.java
@@ -3,15 +3,30 @@ package com.ll.exam;
 import com.ll.exam.article.dto.ArticleDto;
 import com.ll.exam.article.service.ArticleService;
 import com.ll.exam.mymap.MyMap;
+import com.ll.exam.util.Ut;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ArticleServiceTest{
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ArticleServiceTest {
+    // @BeforeAll 붙인 아래 메서드는
+    @BeforeAll
+    public void BeforeAll() {
+        MyMap myMap = Container.getObj(MyMap.class);
+
+        // 모든 DB 처리시에, 처리되는 SQL을 콘솔에 출력
+        myMap.setDevMode(true);
+    }
+
     // @BeforeEach를 붙인 아래 메서드는
     // @Test가 달려있는 메서드가 실행되기 전에 자동으로 실행이 된다.
     // 주로 테스트 환경을 깔끔하게 정리하는 역할을 한다.
@@ -50,35 +65,38 @@ public class ArticleServiceTest{
         // 테이블을 깔끔하게 지워준다.
         myMap.run("TRUNCATE article");
     }
+
     @Test
-    public void 존재한다(){
+    public void 존재한다() {
         ArticleService articleService = Container.getObj(ArticleService.class);
 
         assertThat(articleService).isNotNull();
     }
 
     @Test
-    public void getArticles(){
+    public void getArticles() {
         ArticleService articleService = Container.getObj(ArticleService.class);
 
         List<ArticleDto> articleDtoList = articleService.getArticles();
-
         assertThat(articleDtoList.size()).isEqualTo(3);
     }
 
     @Test
-    public void getArticleById(){
+    public void getArticleById() {
         ArticleService articleService = Container.getObj(ArticleService.class);
-
         ArticleDto articleDto = articleService.getArticleById(1);
 
-        assertThat(articleDto.getId()).isEqualTo(1);
+        assertThat(articleDto.getId()).isEqualTo(1L);
+        assertThat(articleDto.getTitle()).isEqualTo("제목1");
+        assertThat(articleDto.getBody()).isEqualTo("내용1");
+        assertThat(articleDto.getCreatedDate()).isNotNull();
+        assertThat(articleDto.getModifiedDate()).isNotNull();
+        assertThat(articleDto.isBlind()).isFalse();
     }
 
     @Test
     public void getArticlesCount() {
         ArticleService articleService = Container.getObj(ArticleService.class);
-        // selectLong 메서드 이용
         long articlesCount = articleService.getArticlesCount();
 
         assertThat(articlesCount).isEqualTo(3);
@@ -98,5 +116,27 @@ public class ArticleServiceTest{
         assertThat(articleDto.getCreatedDate()).isNotNull();
         assertThat(articleDto.getModifiedDate()).isNotNull();
         assertThat(articleDto.isBlind()).isEqualTo(false);
+    }
+
+    @Test
+    public void modify() {
+        //Ut.sleep(5000);
+
+        ArticleService articleService = Container.getObj(ArticleService.class);
+
+        articleService.modify(1, "제목 new", "내용 new", true);
+
+        ArticleDto articleDto = articleService.getArticleById(1);
+
+        assertThat(articleDto.getId()).isEqualTo(1);
+        assertThat(articleDto.getTitle()).isEqualTo("제목 new");
+        assertThat(articleDto.getBody()).isEqualTo("내용 new");
+        assertThat(articleDto.isBlind()).isEqualTo(true);
+
+        // DB에서 받아온 게시물 수정날짜와 자바에서 계산한 현재 날짜를 비교하여(초단위)
+        // 그것이 1초 이하로 차이가 난다면
+        // 수정날짜가 갱신되었다 라고 볼 수 있음
+        long diffSeconds = ChronoUnit.SECONDS.between(articleDto.getModifiedDate(), LocalDateTime.now());
+        assertThat(diffSeconds).isLessThanOrEqualTo(1L);
     }
 }
